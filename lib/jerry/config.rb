53c24ae86b5f1b59becb6226712f024ffc26a2e7
Boris Bera <bboris@rsoft.ca>
2014-07-05 11:00:29 -0400

    (fix #1) removed Sugar module
    All the methods from the Sugar module have been moved as class methods of the Config class.

--- lib/jerry/config.rb
+++ lib/jerry/config.rb
@@ -1,9 +1,10 @@
-require 'jerry/sugar'
-
 class Jerry
+  class ComponentError < StandardError; end
+
   # Base class for all jerry configs.
   #
-  # It defines all of the instance methods for a config
+  # A config is a class that tells jerry about a set of available
+  # components and how those should be created
   #
   # @abstract Subclass to define a config
   # @example
@@ -11,11 +12,42 @@ class Jerry
   #     component(:service) { MyService.new }
   #     component(:app) { MyApp.new rig(:service) }
   #   end
-  # @see Jerry::Sugar injected class methods
   class Config
-    # Injects Jerry::Sugar into inherited classes
-    def self.inherited(subclass)
-      subclass.send :extend, Jerry::Sugar
+    class << self
+      # @return [Array<Symbol>] list of the components defined by the config
+      def components
+        @components ||= []
+      end
+
+      # Defines a component
+      #
+      # @param [Symbol] name name of the component
+      # @param [Hash] options options hash see supported options
+      # @option options [Symbol] (:single) The scope of the component. Can be either :single or :instance.
+      #   When the scope is :single, only one instance of the component will be created and every call
+      #   to Jerry#rig will return the same instance. When the scope is :instance, every call to Jerry#rig
+      #   will return a new instance.
+      # @yield Block used to instantiate the component. This block in only called when Jerry#rig is called.
+      # @raise [Jerry::ComponentError] when the block is missing or the scope is invalid
+      def component(name, options={}, &block)
+        raise Jerry::ComponentError, "could not define component #{name}, block is missing" if block.nil?
+
+        scope = options[:scope] || :single
+        unless [:single, :instance].include? scope
+          raise Jerry::ComponentError, "could not define component #{name}, scope #{scope} is unknown"
+        end
+
+        define_method name do
+          case scope
+            when :single
+              cache[name] ||= instance_eval(&block)
+            when :instance
+              instance_eval(&block)
+          end
+        end
+
+        components << name
+      end
     end
 
     # @return [Array<Symbol>] list of components defined by the config
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -4,10 +4,11 @@ require 'jerry/config'
 describe Jerry::Config do
   let(:klass) {Class.new(Jerry::Config)}
 
-  it 'should extend the its subclass with Jerry::Sugar when inherited' do
-    expect(klass.singleton_class.included_modules).to include Jerry::Sugar
-    expect(klass).to respond_to :components
-    expect(klass).to respond_to :component
+  describe 'subclass' do
+    it 'should respond to all inherited class methods' do
+      expect(klass).to respond_to :components
+      expect(klass).to respond_to :component
+    end
   end
 
   describe '#components' do
@@ -59,6 +60,113 @@ describe Jerry::Config do
       config.jerry = jerry
 
       expect(config.instance_eval { knows? :something }).to be_truthy
+    end
+  end
+
+  describe 'class methods' do
+    describe '#component' do
+      it 'should create a method with the given name' do
+        klass.component(:my_component) {}
+
+        expect(klass.new).to respond_to(:my_component)
+      end
+
+      it 'should add the name to the list of components' do
+        klass.component(:target) {}
+
+        expect(klass.components).to include(:target)
+      end
+
+      it 'should raise an error when scope is unknown' do
+        expect{klass.component(:target, scope: :not_a_thing) {}}.to raise_error(Jerry::ComponentError)
+      end
+
+      it 'should raise an error when block is missing' do
+        expect{klass.component :target}.to raise_error(Jerry::ComponentError)
+      end
+
+      describe 'defined method' do
+        it 'should have the right self set' do
+          klass.component(:_self) { self }
+          instance = klass.new
+
+          expect(instance._self).not_to be_a Class
+          expect(instance._self).to be_a klass
+          expect(instance._self).to eq(instance)
+        end
+
+        context 'with scope set to :single' do
+          it 'should only call the block once' do
+            call_count = 0
+            klass.component(:target, scope: :single) {call_count += 1}
+            instance = klass.new
+
+            3.times {instance.target}
+
+            expect(call_count).to eq(1)
+          end
+
+          it 'should keep returning the first created component' do
+            call_count = 0
+            klass.component(:target, scope: :single) {call_count += 1}
+            instance = klass.new
+
+            expect(instance.target).to eq(1)
+            expect(instance.target).to eq(1)
+            expect(instance.target).to eq(1)
+          end
+        end
+
+        context 'with scope set to :instance' do
+          it 'should call the block every time' do
+            call_count = 0
+            klass.component(:target, scope: :instance) {call_count += 1}
+            instance = klass.new
+
+            3.times {instance.target}
+
+            expect(call_count).to eq(3)
+          end
+
+          it 'should return a new instance every time' do
+            call_count = 0
+            klass.component(:target, scope: :instance) {call_count += 1}
+            instance = klass.new
+
+            expect(instance.target).to eq(1)
+            expect(instance.target).to eq(2)
+            expect(instance.target).to eq(3)
+          end
+        end
+
+        context 'with no scope' do
+          it 'should only call the block once' do
+            call_count = 0
+            klass.component(:target) {call_count += 1}
+            instance = klass.new
+
+            3.times {instance.target}
+
+            expect(call_count).to eq(1)
+          end
+
+          it 'should keep returning the first created component' do
+            call_count = 0
+            klass.component(:target) {call_count += 1}
+            instance = klass.new
+
+            expect(instance.target).to eq(1)
+            expect(instance.target).to eq(1)
+            expect(instance.target).to eq(1)
+          end
+        end
+      end
+    end
+
+    describe '#components' do
+      it 'should default to an empty array' do
+        expect(klass.components).to eq([])
+      end
     end
   end
 end
