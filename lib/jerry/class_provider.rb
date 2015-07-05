85e3ac3592fdd783a5200493e85b0de84c0f5526
Boris Bera <bboris@rsoft.ca>
2015-07-05 03:06:02 -0400

    Classprovider takes jerry as call method argument

--- lib/jerry/class_provider.rb
+++ lib/jerry/class_provider.rb
@@ -1,27 +1,29 @@
-# A provider that instanciates a given class by collecting constructor arguments
-# through a given jerry instance.
-class ClassProvider
-  # @param jerry [Jerry] a jerry instance
-  # @param klass [Class] class to be instanciated
-  # @param args_spec [Array<Clsss, Symbol, Proc>] specification for the
-  #   constructor arguments. Classes and Symbols are used to collect the
-  #   arguments from the jerry instance. Procs are called to generate arguments.
-  def initialize(jerry, klass, args_spec)
-    @jerry = jerry
-    @klass = klass
-    @args_spec = args_spec
-  end
-
-  # @return An instance of the class given in the constructor
-  def call
-    args = @args_spec.map do |spec|
-      if spec.respond_to? :call
-        spec.call
-      else
-        @jerry[spec]
-      end
+class Jerry
+  # A provider that instanciates a given class by collecting constructor
+  # arguments through a given jerry instance.
+  class ClassProvider
+    # @param klass [Class] class to be instanciated
+    # @param args_spec [Array<Clsss, Symbol, Proc>] specification for the
+    #   constructor arguments. Classes and Symbols are used to collect the
+    #   arguments from the jerry instance. Procs are called to generate
+    #   arguments.
+    def initialize(klass, args_spec)
+      @klass = klass
+      @args_spec = args_spec
     end
 
-    @klass.new(*args)
+    # @param jerry [Jerry] a jerry instance
+    # @return An instance of the class given in the constructor
+    def call(jerry)
+      args = @args_spec.map do |spec|
+        if spec.respond_to? :call
+          spec.call
+        else
+          jerry[spec]
+        end
+      end
+
+      @klass.new(*args)
+    end
   end
 end
--- spec/class_provider_spec.rb
+++ spec/class_provider_spec.rb
@@ -1,6 +1,6 @@
 require 'jerry/class_provider'
 
-describe ClassProvider do
+describe Jerry::ClassProvider do
   let(:jerry) { double 'jerry' }
   let(:klass) { dummy_class }
 
@@ -12,82 +12,82 @@ describe ClassProvider do
 
   describe '#call' do
     it 'should return an instance of the class' do
-      provider = ClassProvider.new jerry, klass, []
+      provider = Jerry::ClassProvider.new klass, []
       instance = double 'instance'
       allow(klass).to receive(:new).and_return(instance)
 
-      expect(provider.call).to eq instance
+      expect(provider.call jerry).to eq instance
     end
 
     it 'should pass constructor arguments in the right order' do
-      provider = ClassProvider.new jerry, klass, [
+      provider = Jerry::ClassProvider.new klass, [
         -> { 'fi' }, -> { 'fo' }, -> { 'fum' }
       ]
 
       expect(klass).to receive(:new).with('fi', 'fo', 'fum')
 
-      provider.call
+      provider.call jerry
     end
   end
 
   describe 'with a class argument' do
     it 'should get the instance from jerry' do
       arg_klass = dummy_class
-      provider = ClassProvider.new jerry, klass, [arg_klass]
+      provider = Jerry::ClassProvider.new klass, [arg_klass]
 
       expect(jerry).to receive(:[]).with(arg_klass)
 
-      provider.call
+      provider.call jerry
     end
 
     it 'should pass the instance from jerry to the constructor' do
-      provider = ClassProvider.new jerry, klass, [dummy_class]
+      provider = Jerry::ClassProvider.new klass, [dummy_class]
       instance = double 'instance'
       allow(jerry).to receive(:[]).and_return(instance)
 
       expect(klass).to receive(:new).with(instance)
 
-      provider.call
+      provider.call jerry
     end
   end
 
   describe 'with a symbol argument' do
     it 'should get the instance from jerry' do
-      provider = ClassProvider.new jerry, klass, [:foobar]
+      provider = Jerry::ClassProvider.new klass, [:foobar]
 
       expect(jerry).to receive(:[]).with(:foobar)
 
-      provider.call
+      provider.call jerry
     end
 
     it 'should pass the instance from jerry to the constructor' do
-      provider = ClassProvider.new jerry, klass, [:foobar]
+      provider = Jerry::ClassProvider.new klass, [:foobar]
       instance = double 'instance'
       allow(jerry).to receive(:[]).with(:foobar).and_return(instance)
 
       expect(klass).to receive(:new).with(instance)
 
-      provider.call
+      provider.call jerry
     end
   end
 
   describe 'with callable argument' do
     it 'should call the callable' do
       callable = double 'callable'
-      provider = ClassProvider.new jerry, klass, [callable]
+      provider = Jerry::ClassProvider.new klass, [callable]
 
       expect(callable).to receive(:call)
 
-      provider.call
+      provider.call jerry
     end
 
     it 'should pass the result of the callable to the constructor' do
       instance = double 'instance'
-      provider = ClassProvider.new jerry, klass, [-> { instance }]
+      provider = Jerry::ClassProvider.new klass, [-> { instance }]
 
       expect(klass).to receive(:new).with(instance)
 
-      provider.call
+      provider.call jerry
     end
   end
 end
